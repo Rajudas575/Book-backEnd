f159d0b8b38a06a32bcd2d4b053bb0430d08e376
Rajudas575 <rajudas575@gmail.com>
2026-01-16 00:42:03 +0530

    change in book.controller

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,11 +1,24 @@
 import Book from '../model/book.model.js'
 
-export const getBook = async(req,res)=>{
-    try {
-        const book = await Book.find();
-        res.status(200).json(book);
-    } catch (error) {
-        console.log("ERROR", error);
-        res.status(500).json(error);
-    }
-}
+// export const getBook = async(req,res)=>{
+//     try {
+//         const book = await Book.find();
+//         res.status(200).json(book);
+//     } catch (error) {
+//         console.log("ERROR", error);
+//         res.status(500).json(error);
+//     }
+// }
+
+export const getBook = async (req, res) => {
+  try {
+    const book = await Book.find();
+    res.status(200).json(book);
+  } catch (error) {
+    console.error("❌ BOOK ERROR:", error.message);
+    res.status(500).json({
+      message: error.message,
+      name: error.name
+    });
+  }
+};
